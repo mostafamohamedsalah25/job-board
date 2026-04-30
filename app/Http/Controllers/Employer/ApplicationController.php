4cb7adf44fd9b1fa51744ee7df050c40b5cc3ac4
mostafamohamedsalah25 <mostafamohamedsalah25@gmail.com>
2026-05-01 01:44:06 +0300

    fix(ApplicationController): import Application model for application status updates

--- app/Http/Controllers/Employer/ApplicationController.php
+++ app/Http/Controllers/Employer/ApplicationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Employer;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Application;
 
 class ApplicationController extends Controller
 {
